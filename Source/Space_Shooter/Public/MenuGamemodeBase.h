5da2f70eb3aaeab46dfea8ab261782c0b8dd20ab
SOprograming <jobbsimen13@gmail.com>
2023-03-07 16:50:11 +0100

    HUD and Startmenu
    Added loss and win screan, added helth img's and added a start Menu

---
+++ Source/Space_Shooter/Public/MenuGamemodeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MenuGamemodeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACE_SHOOTER_API AMenuGamemodeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
